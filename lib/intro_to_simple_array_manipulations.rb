8df82fd4c2bce4a2abfab4bc94fb4c22c17cbd33
Will Reeves <wrhreeves@gmail.com>
2020-04-08 19:01:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
 def using_push(arr, string)
   arr.push(string)
 end
+
+def using_unshift(arr, string)
+  arr.unshift(string)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr, arg)
+  arg.times do {arr.pop}
+end
